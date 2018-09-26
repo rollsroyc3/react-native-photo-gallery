c8137e6b82940267b2efad58c742bc684cbdfdf9
Andrew Royce <andrew.w.royce@gmail.com>
2018-09-26 11:16:59 -0400

    missing import

--- src/Pagination/AddThumb.js
+++ src/Pagination/AddThumb.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Image } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 export const SIZE = 80;
